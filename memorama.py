31443671f404696f73b5a90e500fc668fc2238ae
salypimientado <juanpablosalazar63@hotmail.com>
2019-10-04 16:55:49 -0500

    oof v1.1

--- memorama.py
+++ memorama.py
@@ -2,6 +2,24 @@ import math
 import random
 
 
+
+
+
+
+def juego():
+
+    nums = matrixv_rand(matrix_c(6,6))
+
+    noc = matrix2_c(6,6)
+
+    for line in noc:
+        print(*line)
+    for a in range(len(noc)):
+        while "-" in noc[a]:
+            presbut(nums, noc)
+
+    print("Ganaste!")
+
 def matrix_c(s1, s2):
     matrix = [[0 for x in range(s1)] for y in range(s2)]
     return matrix
@@ -12,7 +30,8 @@ def matrix2_c(s1, s2):
 
 def matrixv_rand(matrix):
 
-    ebic = []
+    v1 = 0
+    v2 = 1
 
     li = list(range(1, 19))
     for x in range(1,19):
@@ -23,53 +42,34 @@ def matrixv_rand(matrix):
         for m in range(len(matrix[x-1])):
             matrix[x-1][m-1] =  li[0]
             li.pop(0)
-        matrix[x-1].append("\n")
-
-
-    '''  while len(li) !=0:
-
-        col = nri[m] % 6
-        ren = math.floor(nri[m] / 6)
-        matrix[ren][col-1] = li[a]
-        print("a", a ,"len li", len(li), "m", m, "nri len", len(nri))
-        li.pop(a)
-        nri.pop(m-1)
-        
-        
-
-
-
-        while len(lii) != 0:
-        
-            a = random.choice(range(0,len(lii)))
-            m = random.choice(range(0, len(nrii)))
-            col = nrii[m] % 6
-            ren = math.floor(nrii[m] / 6)
-            matrix[ren-1][col] = lii[a]
-            lii.pop(a)
-            nrii.pop(m-1)
-
-         if len(l) == 1:
-
-            matrix[math.floor(l[0] / 6)][l[0] % 6 - 1] = l[0]
-            l.pop(0)
-        else:
-            temp = random.randint(0, len(l))
-            print("temp: ", temp)
-            print(len(l))
-            num = l[temp-1]
-            col = num % 6
-            ren = math.floor(num / 6)
-            print("a", temp, "l", len(l), "r", ren,"c", col)
-            matrix[ren-1][col-1] = num
-            if len(l) == 1:
-                l.pop(0)
-            else:
-                l.pop(temp-1)'''
     return matrix
 
+def presbut(m1, m2):
+    x1 = int(input("Introduzca la columna de su eleccion\n"))
+    y1 = int(input("Introduzca la fila de su eleccion\n"))
+    n1 = m1[y1-1][x1-1]
+    m2[y1-1][x1-1] = m1[y1-1][x1-1]
+    for line in m2:
+        print(*line)
+    x2 = int(input("Introduzca la columna de su segunda eleccion\n"))
+    y2 = int(input("Introduzca la columna de su secunda eleccion\n"))
+    n2 = m1[y2-1][x2-1]
+    if n1 == n2:
+        m2[y2-1][x2-1] = m1[y2-1][x2-1]
+        for line in m2:
+            print(*line)
+
+    else:
+
+        m2[y2 - 1][x2 - 1] = m1[y2 - 1][x2 - 1]
+        for line in m2:
+            print(*line)
+        print("\n")
+        m2[y1 - 1][x1 - 1] = "-"
+        m2[y2 - 1][x2 - 1] = "-"
 
 
+    return m2
 
-print(matrixv_rand(matrix_c(6, 6)))
-print(matrix2_c(6,6))
+
+juego()
